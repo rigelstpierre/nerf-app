88bd1d75f3821a864f6b6e54833aea27430b2184
rigelstpierre <rigel@ministrycentered.com>
2012-06-13 11:42:37 -0700

    adds support for ENV Vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     IdentitiesController.action(:new).call(env)
   }
 
-  provider PlanningCenterOauth, "24WompkcH9aryNVIb2LY", "yxv9MtF56OijiQ7ghz5Nqalm1ORHLE5SW8cPHGrN", :client_options => {:site => "https://www.planningcenteronline.com"}, :name => 'planning_center'
+  provider PlanningCenterOauth, ENV['PCO_KEY'], ENV['PCO_SECRET'], :client_options => {:site => "https://www.planningcenteronline.com"}, :name => 'planning_center'
 end
+
+# Localhost: "24WompkcH9aryNVIb2LY", "yxv9MtF56OijiQ7ghz5Nqalm1ORHLE5SW8cPHGrN"
+# Production: "XQGnO9JtI6foj3l4bzkh", "rGwdFwzlejNtEgqQY4MP9LM4fU5lG96A5a9ggWbz"
+
+
+# export PCO_KEY=24WompkcH9aryNVIb2LY
+
