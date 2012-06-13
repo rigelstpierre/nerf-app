a6b4d07847c6bb1a84e88ca4169bbf960f6fdaa0
rigelstpierre <rigel@ministrycentered.com>
2012-06-12 18:15:13 -0700

    added oAuth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_player
+
+  private
+
+  def current_player
+    @current_player ||= Player.find(session[:player_id]) if session[:player_id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ NerfApp::Application.routes.draw do
   root to: "players#index"
 
   match "/auth/:provider/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", :as => :signout
 end
