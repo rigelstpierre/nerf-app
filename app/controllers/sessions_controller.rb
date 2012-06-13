990da2c9273572b6a2cd0259914a31e1f9f6ce88
rigelstpierre <rigel@ministrycentered.com>
2012-06-13 13:46:08 -0700

    fixes 500 error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    player = Player.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Player.create_with_omniauth(auth)
+    player = Player.find_by_provider_and_uid(auth["provider"], auth["uid"].to_s) || Player.create_with_omniauth(auth)
     session[:player_id] = player.id
     redirect_to root_url, :notice => "Signed in!"
   end
---
+++ db/migrate/20120613203754_change_user_id_to_interger.rb
@@ -0,0 +1,7 @@
+class ChangeUserIdToInterger < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
